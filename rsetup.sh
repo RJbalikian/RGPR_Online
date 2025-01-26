5454d44e6749765325c4915618a17a12a9410d4e
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2025-01-26 13:19:49 -0600

    Initial commit

---
+++ rsetup.sh
@@ -0,0 +1,4 @@
+# Setup scripts to run in bash terminal
+# Do bash installs first
+sudo apt install libfontconfig1-dev libgdal-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+sudo apt-get install libcairo2-dev tcl tk libudunits2-dev libhdf5-dev
